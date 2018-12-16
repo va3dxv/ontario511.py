5623ae18eeb0e4755db72eca3db7a48c0ac4025a
va3dxv <45865432+va3dxv@users.noreply.github.com>
2018-12-15 20:29:56 -0500

    Create ontario511.py

---
+++ ontario511.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3.5
+#
+# ontario511.py
+# 
+# 15/12/2918
+# 
+# Brian Graves - VA3DXV
+#
+# va3dxv@gmail.com
+# 
+# To Do:
+# 1) Integrate voicerss.org API for text to speech
+#
+#####################################################################################################
+import requests
+
+roadreports = requests.get (
+        "https://511on.ca/api/v2/get/roadconditions"
+).json()
+
+for items in roadreports:
+        area = (items["AreaName"])
+        location = (items["LocationDescription"] .split("|")[0])
+        condition = (items["Condition"])
+        roadname = (items["RoadwayName"])
+        vis = (items["Visibility"])
+        poly = (items["EncodedPolyline"])
+        if (area == "Eastern" and roadname == "417" or roadname == "138" or roadname == "416"):
+            if ("Quebec" in location or "Arnprior" in location): continue
+            print("Highway",roadname,location,".",condition[0],".","visibility is",vis,"..")
+
+
+#####################################################################################################
+# output
+# Highway 138 From Cornwall to Highway 417 .. Bare and dry road .. visibility is Good
+# Highway 416 From Highway 401 to Fallowfield Road .. Bare and dry road .. visibility is Good
+# Highway 417 From Highway 138 to Gloucester .. Bare and dry road .. visibility is Good
+# Highway 417 From Gloucester to Fitzroy .. Bare and dry road .. visibility is Good
+# Highway 416 From Fallowfield Road to Highway 417 .. Bare and dry road .. visibility is Good
+#
+#EOF
