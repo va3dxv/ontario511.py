b728e9fd5e48f6b67053bbd144ae32298e4ab591
va3dxv <va3dxv@CALLOUS.disregard.net>
2018-12-16 02:51:12 -0500

    added file write and tts_audio

--- ontario511.py
+++ ontario511.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3.5
 #
 # ontario511.py
-# 
+#
 # 15/12/2018
-# 
+#
 # Brian Graves - VA3DXV
 #
 # va3dxv@gmail.com
-# 
-# To Do:
-# 1) Integrate voicerss.org API for text to speech
 #
 #####################################################################################################
 import requests
+import subprocess
+import shlex
 
 roadreports = requests.get (
         "https://511on.ca/api/v2/get/roadconditions"
 ).json()
 
+
+file=open("/tmp/ontario511.txt","+w")
+
+file.write("Current highway conditions..\r\n")
 for items in roadreports:
         area = (items["AreaName"])
         location = (items["LocationDescription"] .split("|")[0])
@@ -26,14 +29,25 @@ for items in roadreports:
         vis = (items["Visibility"])
         if (area == "Eastern" and roadname == "417" or roadname == "138" or roadname == "416"):
             if ("Quebec" in location or "Arnprior" in location): continue
-            print("..Highway %s %s. %s. Visibility is %s." % (roadname,location,condition[0],vis))
-        
+            file.write("..Highway %s %s. %s. Visibility is %s. \r\n" % (roadname,location,condition[0],vis))
+file.write("End of report.\r\n")
+file.close()
+
+subprocess.call(shlex.split("/usr/local/sbin/tts_audio.sh /tmp/ontario511.txt"))
+subprocess.call(shlex.split("rm -f /tmp/ontario511.txt"))
+subprocess.call(shlex.split("mv /tmp/ontario511.ul /etc/asterisk/custom"))
+
+
 #####################################################################################################
-# output
+#
+# output:
+#
+# Current highway conditions..
 # Highway 138 From Cornwall to Highway 417 . Bare and dry road . visibility is Good ..
 # Highway 416 From Highway 401 to Fallowfield Road . Bare and dry road . visibility is Good ..
 # Highway 417 From Highway 138 to Gloucester . Bare and dry road . visibility is Good ..
 # Highway 417 From Gloucester to Fitzroy . Bare and dry road . visibility is Good ..
 # Highway 416 From Fallowfield Road to Highway 417 . Bare and dry road . visibility is Good ..
+# End of report.
 #
 #EOF
